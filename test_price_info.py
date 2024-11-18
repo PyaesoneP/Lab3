b98653096b0350f9983b7d5771349a99cbac6f0c
Pyae Sone <pyaesone.perfect2014@gmail.com>
2024-11-18 17:40:15 +0800

    Finish cost_of_fruit and test

--- test_price_info.py
+++ test_price_info.py
@@ -4,3 +4,10 @@ def test_total_cost_shopping():
     test_price = 46.75
     result_price = price_info.total_cost_shopping()
     assert(result_price==test_price)
+
+def test_cost_of_fruit():
+    test_cost = 12.0
+    input_fruit = 'apple'
+    input_quantity = 10
+    result_cost = price_info.cost_of_fruits(input_fruit, input_quantity)
+    assert(result_cost==test_cost)
