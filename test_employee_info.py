7a7677136e8558455b369a97202b1388a241ba82
Pyae Sone <pyaesone.perfect2014@gmail.com>
2024-11-18 18:02:55 +0800

    Test get_employees_by_age_range

---
+++ test_employee_info.py
@@ -0,0 +1,14 @@
+import employee_info
+
+def test_get_employees_by_age_range():
+    result = []
+    test_employees = [
+        {"name": "John", "age": 30, "department": "Sales", "salary": 50000},
+        {"name": "Jane", "age": 25, "department": "Marketing", "salary": 60000},
+        {"name": "Chloe",  "age": 35, "department": "Engineering", "salary": 70000},
+        {"name": "Mike", "age": 32, "department": "Engineering", "salary": 65000}
+        ]
+    input_lower_age = 24
+    input_upper_age = 40
+    result = employee_info.get_employees_by_age_range(input_lower_age,input_upper_age)
+    assert(result==test_employees)
